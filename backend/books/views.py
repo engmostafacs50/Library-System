38bc70e1321312ab2cfac67dddebcaac23538e12
Mohamed Wael <nostergo@gmail.com>
2026-05-10 23:04:27 +0300

    testing

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, filters
+from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_staff
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset           = Book.objects.all()
+    serializer_class   = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends    = [filters.SearchFilter]
+    search_fields      = ['title', 'author', 'genre']
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset           = Book.objects.all()
+    serializer_class   = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class BookByGenreView(generics.ListAPIView):
+    serializer_class   = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        genre = self.kwargs['genre']
+        return Book.objects.filter(genre__iexact=genre)
